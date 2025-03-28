45ecd3eb510c61bfbdb2a70130fef46e65caa083
Langdon Douglas <langdon.douglas26@gmail.com>
2025-03-28 06:05:11 -0500

    1. Create HTML skeleton 2. Created CSS classlists for chessboard and spaces 3.created ChessBoard class that calls createBoard 4. createBoard function    a. create rows    b.create columns    c.style spaces depending on location    d. add ID to spaces    e.  add spaces to the chessboard

---
+++ knightMoves.js
@@ -0,0 +1,38 @@
+class ChessBoard{ 
+    constructor(containerId){ 
+        this.container = document.querySelector(containerId); 
+        this.board = []
+        this.createBoard();
+    }
+    
+    createBoard() { 
+        
+        for(let row = 0; row < 8; row++) { 
+            //create row
+            this.board[row] = [];
+            for(let col = 0; col < 8; col++){ 
+                //create space divs 
+                const space = document.createElement('div'); 
+                //add general styling
+                space.classList.add('boardSpace');
+                //create alternating space styling
+                if((row + col) % 2 == 0){
+                    space.classList.add('darkSpace'); 
+                }
+                //assign id to each space
+                space.id = `space-${row}-${col}`; 
+                //adding the spaces to the chessboard
+                this.container.appendChild(space);
+                //assign row and columns to space
+                this.board[row][col] = space; 
+            }
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded" , () => {
+    const testCB = new ChessBoard("#container"); 
+    console.log(testCB);
+})
+
+console.log("script is loading");
